7ca4205101a88029f581a71a6165dbf7fd314a25
void <git.email.address@googlemail.com>
2013-06-21 16:41:31 +0200

    TypeScript test worked

---
+++ TypeScriptTest/socketTest.ts
@@ -0,0 +1,21 @@
+function write(line : string) {
+    var output = document.getElementById('output');
+    output.innerHTML += line + "\n";
+}
+
+function runTest() {
+    var url = 'ws://127.0.0.1:81/';
+    write('Connecting');
+    var socket = new WebSocket(url);
+    socket.onopen = function () {
+        write('Connected');
+    };
+    socket.onmessage = function (event) {
+        write('Received: ' + event.data);
+    };
+    socket.onclose = function () {
+        write('Disconnected');
+    };
+}
+
+runTest();
