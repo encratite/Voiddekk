0e1b55c492d45ad04ea03621a1392fad196a4f25
void <git.email.address@googlemail.com>
2013-06-24 13:03:35 +0200

    Added an SHA3 test

---
+++ SHA3Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SHA3Test
+{
+	class Program
+	{
+		static void Main(string[] arguments)
+		{
+			string input = "The quick brown fox jumps over the lazy dog";
+			var hash = new SHA3Managed();
+			byte[] output = hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+		}
+	}
+}
