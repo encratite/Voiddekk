821fe41836dbafd7240baee97cca723a20854f61
void <git.email.address@googlemail.com>
2013-06-24 02:53:06 +0200

    Added a JSON test

---
+++ JSONTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace JSONTest
+{
+	class Call
+	{
+		public string Function;
+		public object[] Arguments;
+
+		public Call()
+		{
+		}
+
+		public Call(string function, params object[] arguments)
+		{
+			Function = function;
+			Arguments = arguments;
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Call call = new Call("function", 1, 2, 3, "string", false, true, null);
+			JavaScriptSerializer serialiser = new JavaScriptSerializer();
+			string serialisedData = serialiser.Serialize(call);
+			Call output = serialiser.Deserialize<Call>(serialisedData);
+		}
+	}
+}
