df31b0204584f3e02a096e416cbe0c92423f2a2f
void <git.email.address@googlemail.com>
2013-07-10 14:25:58 +0200

    Preparing for the new card types

--- Game/UnitType.cs
+++ Game/UnitType.cs
@@ -3,17 +3,9 @@ using System.Xml.Serialization;
 
 namespace Game
 {
-	public class UnitType
+	[XmlType(TypeName = "Unit")]
+	public class UnitType: CardType
     {
-		// The ID is generated automatically later
-		[XmlIgnore]
-		public int? Id;
-		public string Name { get; set; }
-		// Used for image paths
-		public string InternalName { get; set; }
-		// A limit of null means the default limit
-		public int? Limit { get; set; }
-		public int Resources { get; set; }
 		public int Damage { get; set; }
 		public int Life { get; set; }
 		public List<UnitFlag> Flags { get; set; }
--- Tests/ConfigurationTest/Program.cs
+++ Tests/ConfigurationTest/Program.cs
@@ -1,24 +1,39 @@
-﻿using Game;
+﻿using System;
+
+using Game;
 using Nil;
 
 namespace ConfigurationTest
 {
 	class Program
 	{
-		static void Main(string[] arguments)
+		static string ConfigurationPath = "Configuration.xml";
+
+		static void ReadTest()
 		{
-			Serialiser<Configuration> serialiser = new Serialiser<Configuration>("Configuration.xml");
+			Serialiser<Configuration> serialiser = new Serialiser<Configuration>(ConfigurationPath);
+			Configuration configuration = serialiser.Load();
+			Console.WriteLine("Limit: {0}", configuration.Units[0].Limit);
+		}
+
+		static void WriteTest()
+		{
+			Serialiser<Configuration> serialiser = new Serialiser<Configuration>(ConfigurationPath);
 			Configuration configuration = new Configuration();
 			UnitType unit = new UnitType();
 			unit.Name = "Name";
 			unit.InternalName = "FactionName";
-			unit.Limit = 4;
 			unit.Damage = 1;
 			unit.Life = 1;
 			UnitFlag flag = new UnitFlag(UnitFlagType.Regenerate, 1);
 			unit.Flags.Add(flag);
 			configuration.Units.Add(unit);
 			serialiser.Store(configuration);
+		}
+
+		static void Main(string[] arguments)
+		{
+			ReadTest();
 		}
 	}
 }
